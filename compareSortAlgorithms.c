d06aac0090cf1a3672905240ecd39f1adb9415ae
rbasey <ra198820@ucf.edu>
2024-03-23 23:45:13 -0400

    Completed Assignment

--- compareSortAlgorithms.c
+++ compareSortAlgorithms.c
@@ -1,6 +1,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
 
@@ -25,24 +26,121 @@ size_t Size(void* ptr)
 {
 	return ((size_t*)ptr)[-1];
 }
+//function to swap two integers
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+//this function needed for heap sort
+void heapify(int pData[], int n, int i) {
+	int large = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
 
+	if(right < n && pData[right] > pData[large]) {
+		large = right;
+	}
+
+	if(left < n && pData[left] > pData[large]) {
+		large = left;
+	}
+
+	if(large != i) {
+		swap(&(pData[i]), &(pData[large]));
+		heapify(pData, n, large);
+	}
+}
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
-void heapSort(int arr[], int n)
+void heapSort(int pData[], int i, int n)
 {
+	for(i = n/2; i >=0; i--) {
+		heapify(pData, n, i);
+	}
+	for(i = n; i >= 0; i--) {
+		swap(&(pData[0]), &(pData[i]));
+		heapify(pData, i, 0);
+	}
 }
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
-	
-}
+	if(l < r) {
+		//m is the mid point
+		int m = (l+r)/2;
+		//first half
+		mergeSort(pData, l, m);
+		//second half
+		mergeSort(pData, m+1, r);
+		
+		//merge two halfs
+		int i, j, k;
+		int n1 = m - l + 1;
+		int n2 = r - m;
+		//temp left array
+		int *left = Alloc(sizeof(int)*n1);
+		//temp right array
+		int *right = Alloc(sizeof(int)*n2);
+		//copy data to left
+		for(i = 0; i < n1; i++) {
+		left[i] = pData[l+i];
+		}
+		//copy data to right
+		for(j = 0; j < n2; j++) {
+		right[j] = pData[m+1+j];
+		}
+		//merge back into pData
+		i = 0;
+		j = 0;
+		k = l;
+		while(i < n1 && j < n2) {
+			if(left[i] <= right[j]) {
+				pData[k] = left[i];
+				i++;
+			} else {
+				pData[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+		//copy remaining elements of left
+		while(i < n1) {
+			pData[k] = left[i];
+			i++;
+			k++;
+		}
+		//copy remaining elements of right
+		while(j < n2) {
+			pData[k] = right[j];
+			j++;
+			k++;
+		}
 
+		//free the temp arrays 
+		DeAlloc(left);
+		DeAlloc(right);
+	}
+}
 // implement insertion sort
 // extraMemoryAllocated counts bytes of memory allocated
 void insertionSort(int* pData, int n)
 {
+	int i, item, j;
+	for(i = 1; i < n; i++){
+		item = pData[i];
+		for(j=i-1; j >= 0; j--) {
+			if(pData[j] > item) {
+				//shift items
+				pData[j+1] = pData[j];
+			} else {
+				break;
+			}
+		}
+		pData[j+1] = item;
+	}
 	
 }
 
@@ -50,6 +148,16 @@ void insertionSort(int* pData, int n)
 // extraMemoryAllocated counts bytes of extra memory allocated
 void bubbleSort(int* pData, int n)
 {
+	int i, j;
+	//compare adjacent items
+	for(i = 0; i < n-1; i++) {
+		for(j = 0; j < n-i-1; j++){
+			if(pData[j] > pData[j+1]) {
+				//swap when element to the right is larger
+				swap(&(pData[j]), &(pData[j+1]));
+			}
+		}
+	}
 	
 }
 
@@ -57,6 +165,21 @@ void bubbleSort(int* pData, int n)
 // extraMemoryAllocated counts bytes of extra memory allocated
 void selectionSort(int* pData, int n)
 {
+	int i, j, min_idx;
+	//increment i
+	for(i = 0; i < n-1; i++) {
+		//look for the minimum element
+		min_idx = i;
+		for(j=i+1; j < n; j++) {
+			if(pData[j]<pData[min_idx]) {
+				min_idx = j;
+			}
+		}
+		//swap the minimum element and element at i
+		if(min_idx != i) {
+		swap(&(pData[i]), &(pData[min_idx]));
+		}
+	}
 	
 }
 
@@ -66,12 +189,23 @@ int parseData(char *inputFileName, int **ppData)
 	FILE* inFile = fopen(inputFileName,"r");
 	int dataSz = 0;
 	*ppData = NULL;
+	int i, n, *data;
 	
 	if (inFile)
 	{
 		fscanf(inFile,"%d\n",&dataSz);
 		*ppData = (int *)Alloc(sizeof(int) * dataSz);
 		// Implement parse data block
+		if (*ppData == NULL) {
+			printf("Cannot allocate memory\n");
+			exit(-1);
+		}
+		for(i = 0; i < dataSz; ++i) {
+			fscanf(inFile, "%d", &n);
+			data = *ppData + i;
+			*data = n;
+		}
+		fclose(inFile);
 	}
 	
 	return dataSz;
